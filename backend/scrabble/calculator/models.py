f2252e53d3355dc4a494122157db191a2647ef6c
unknown <mu-za-mir@hotmail.com>
2020-09-17 17:54:05 +0200

    feat(model):Added ScoreTable and UserEntry Model with test

--- backend/scrabble/calculator/models.py
+++ backend/scrabble/calculator/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+""" Model for Scrabble Calculator
+    """
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class ScoreTable(models.Model):
+    """ScoreTable
+    """
+    letter = models.CharField(max_length=1)
+    value = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(20)])
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.letter
+
+
+class UserEntry(models.Model):
+    """UserEntry with score and no user model attached
+    """
+    name = models.CharField(max_length=120)
+    word = models.CharField(max_length=120)
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.name + "-" + self.word + ":" + str(self.score)
--- backend/scrabble/calculator/tests.py
+++ backend/scrabble/calculator/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from calculator import models
 
-# Create your tests here.
+
+def create_entry(name="muzamir", word="Intelligent", score=10):
+    """Create an entry on User Entry Model
+
+    Args:
+        name (str, optional): [description]. Defaults to "muzamir".
+        word (str, optional): [description]. Defaults to "Intelligent".
+        score (int, optional): [description]. Defaults to 10.
+
+    Returns:
+        [str]: [model default string]
+    """
+
+    return models.UserEntry.objects.create(name=name, word=word, score=score)
+
+
+def create_score_table_entry(letter="A", value=10):
+    """create_score_table_entry 
+
+    Args:
+        letter (str, optional): [description]. Defaults to "A".
+        value (int, optional): [description]. Defaults to 10.
+    Returns:
+    [str]: [model default string]
+    """
+
+    return models.ScoreTable.objects.create(letter=letter, value=value)
+
+
+class ModelTests(TestCase):
+    """Test Model Validity
+    """
+
+    def test_create_one_entry(self):
+        """Test create on entry"""
+
+        entry = create_entry()
+
+        self.assertEqual(str(entry), "muzamir-Intelligent:10")
+
+    def test_create_scoretable_entry(self):
+        """Test scoretable Entry"""
+        score_entry = create_score_table_entry()
+
+        self.assertEqual(str(score_entry), "A")
