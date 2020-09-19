2b640345b3e63a3f83f866bbdf683dd7c65eb5f9
Muzamir <mu-za-mir@hotmail.com>
2020-09-19 11:31:26 +0200

    style(score): space to score typography

--- frontend/scrabble/src/components/score.js
+++ frontend/scrabble/src/components/score.js
@@ -4,7 +4,7 @@ import { Typography } from "@material-ui/core";
 function ScoreCard(props) {
   return (
     <React.Fragment>
-      <Typography variant="h4">Your Score :{props.score}</Typography>
+      <Typography variant="h4">Your Score : {props.score}</Typography>
     </React.Fragment>
   );
 }
