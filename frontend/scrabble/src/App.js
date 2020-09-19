d8953ea61e7550dca2768c1eef4835582a564f27
Muzamir <mu-za-mir@hotmail.com>
2020-09-19 11:01:47 +0200

    feat(App): Add ScoreCard, InputForm, SimpleTable in App
    userEntry and list passed down to child component props

--- frontend/scrabble/src/App.js
+++ frontend/scrabble/src/App.js
@@ -1,22 +1,44 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUserEntryList } from "./actions/userEntryActions";
-import "./App.css";
 
-function App() {
+import ScoreCard from "./components/score";
+import InputForm from "./containers/input";
+import SimpleTable from "./components/table";
+
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import "./App.css";
+const useStyles = makeStyles({
+  form: {
+    padding: "4em 4em",
+  },
+});
+
+function App(props) {
   const dispatch = useDispatch();
+  const classes = useStyles();
+  const userEntry = useSelector((state) => state.userEntryReducer);
 
   useEffect(() => {
     dispatch(fetchUserEntryList());
-  }, []);
+    // eslint-disable-next-line
+  }, [userEntry.score]);
   const list = useSelector((state) => state.userEntryReducer.list);
 
   return (
     <div className="App">
-      {list.map((obj) => {
-        return <p>{obj.word}</p>;
-      })}
-      {console.log(list)}
+      <Grid container>
+        <Grid item xs={12} className={classes.form}>
+          <ScoreCard score={userEntry.score} />
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <InputForm />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <SimpleTable list={list} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
